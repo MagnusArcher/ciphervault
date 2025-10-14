1446a231c2f4ff193e63e893bf315ccd2b0d253c
Magnus Archer <yzxd8762@gmail.com>
2025-10-14 08:30:07 +0330

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,14 @@
+from .password_gen import generate_password
+from .password_check import check_password_strength
+from .encryptor import encrypt_text
+from .decryptor import decrypt_text
 
+__all__ = [
+    'generate_password',
+    'check_password_strength',
+    'encrypt_text',
+    'decrypt_text'
+]
+
+__version__ = '1.0.0'
+__author__ = 'Magnus Archer'
