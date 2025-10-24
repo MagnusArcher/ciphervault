5acf8f1c79476cee8c89f2f22053b130277c0d8a
Magnus Archer <yzxd8762@gmail.com>
2025-10-24 19:18:52 +0330

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,14 +2,14 @@ from .password_gen import generate_password
 from .password_check import check_password_strength
 from .encryptor import encrypt_text
 from .decryptor import decrypt_text
-from .clipboard_utils import ask_copy
+from .clipboard_utils import copy_to_clipboard
 
 __all__ = [
     'generate_password',
     'check_password_strength',
     'encrypt_text',
     'decrypt_text',
-    'ask_copy'
+    'copy_to_clipboard'
 ]
 
 __version__ = '1.0.0'
